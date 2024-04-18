1e7f5a6591218e06a34c58cf56c00c0eb643bb34
ryanbuck182@ <ryanbuck182@gmail.com>
2024-04-18 12:08:43 -0400

    Add Move struct

---
+++ Console Chess/Move.cpp
@@ -0,0 +1,6 @@
+#include "Move.h"
+
+Move::Move(int startSquare, int endSquare) {
+	this->startSquare = startSquare;
+	this->endSquare = endSquare;
+}
---
+++ Console Chess/Move.h
@@ -0,0 +1,10 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+struct Move {
+	Move(int, int);
+	int startSquare;
+	int endSquare;
+};
+
+#endif
