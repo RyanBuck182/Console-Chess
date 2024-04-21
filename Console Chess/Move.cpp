ee680beeba2c4fdb8297f05c03e52814b69b2341
ryanbuck182@ <ryanbuck182@gmail.com>
2024-04-21 16:06:00 -0400

    Classify moves by MoveType

--- Console Chess/Move.cpp
+++ Console Chess/Move.cpp
@@ -4,8 +4,17 @@
 Move::Move(Square* startSquare, Square* endSquare) {
 	this->startSquare = startSquare;
 	this->endSquare = endSquare;
+	moveType = Standard;
+}
+
+Move::Move(Square* startSquare, Square* endSquare, MoveType moveType) {
+	this->startSquare = startSquare;
+	this->endSquare = endSquare;
+	this->moveType = moveType;
 }
 
 bool Move::operator==(const Move& move) const {
-	return startSquare == move.startSquare && endSquare == move.endSquare;
+	return startSquare == move.startSquare
+		&& endSquare == move.endSquare
+		&& moveType == move.moveType;
 }
--- Console Chess/Move.h
+++ Console Chess/Move.h
@@ -1,13 +1,17 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+enum MoveType { Standard, Capture, DoublePawn, EnPassant, Castle };
+
 class Square;
 
 struct Move {
 	Move(Square*, Square*);
+	Move(Square*, Square*, MoveType);
 
 	Square* startSquare;
 	Square* endSquare;
+	MoveType moveType;
 
 	bool operator==(const Move& move) const;
 };
