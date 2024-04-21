cde447fd04bfbe8b572ed5a25bd8ebe463a2f3e0
ryanbuck182@ <ryanbuck182@gmail.com>
2024-04-21 01:06:57 -0400

    Change access specifiers

--- Console Chess/Pawn.h
+++ Console Chess/Pawn.h
@@ -4,11 +4,12 @@
 #include "Piece.h"
 
 class Pawn : public Piece {
+public:
 	Pawn(Square*, bool);
 
-	bool hasMoved;
-
 	std::vector<Move> getValidMoves(const Board&) const override;
+private:
+	bool hasMoved;
 };
 
 #endif
