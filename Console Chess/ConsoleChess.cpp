6da56a5ba31b5ff852f414e3f5756f4978f4f9a3
ryanbuck182@ <ryanbuck182@gmail.com>
2024-04-22 00:18:32 -0400

    Add gameplay testing

--- Console Chess/ConsoleChess.cpp
+++ Console Chess/ConsoleChess.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cctype>
+
 #include "ConsoleChess.h"
 
 using namespace std;
@@ -6,9 +8,66 @@ using namespace std;
 int main() {
 	Board board;
 
-	cout << board;
-	cout << "\nPress enter to continue...";
-	cin.get();
+	while (board.state != Board::Win) {
+		cout << board;
+		
+		Move* move = nullptr; //set move by overloading extraction operator
+		while (move == nullptr) {
+			Square* startSquare = nullptr;
+			Square* endSquare = nullptr;
+			
+			cout << '\n' << ((board.state == Board::WhiteToPlay) ? "White" : "Black") << " To Play\n";
 
-	board.getSquareFromId(0)->piece->computeValidMoves(board);
+			while (startSquare == nullptr) {
+				char column;
+				int row;
+
+				cout << "  Start Square: ";
+				
+				cin >> column;
+				cin >> row;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+
+				int id = (row - 1) * Board::BOARD_LENGTH + (static_cast<int>(tolower(column) - static_cast<int>('a')));
+
+				try {
+					startSquare = board.getSquareFromId(id);
+				} catch (const char* error) {
+					cout << "Invalid input! Please input a valid square (e.g. a1).\n\n";
+				}
+			}
+
+			while (endSquare == nullptr) {
+				char column;
+				int row;
+
+				cout << "    End Square: ";
+
+				cin >> column;
+				cin >> row;
+				cin.clear();
+				cin.ignore(INT_MAX, '\n');
+
+				int id = (row - 1) * Board::BOARD_LENGTH + (static_cast<int>(tolower(column) - static_cast<int>('a')));
+
+				try {
+					endSquare = board.getSquareFromId(id);
+				} catch (const char* error) {
+					cout << "Invalid input! Please input a valid square (e.g. a1).\n\n";
+				}
+			}
+
+			move = new Move(startSquare, endSquare);
+
+			try {
+				board.makeMove(move);
+			} catch (const char* error) {
+				move = nullptr;
+				cout << "Invalid move! Please input a legal move.\n";
+			}
+		}
+
+		cout << "\n\n";
+	}
 }
